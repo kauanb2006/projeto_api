18eb7f8088a9fa3a68cb66d0205c1e41fc5fdceb
kauan <kauanbarbosalopes@gmail.com>
2024-05-07 10:31:23 -0300

    novas rotas

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
- # Função que retorna o JSON com status 200 e a mensagem "API do SEU_NOME_COMPLETO".
-  return {"status": 200, "message": "API do {}".format([ "Kauan Barbosa Lopes"])}
-@app.route("/aleatorios")
-def aleatorios():
-  import random
-  a = random.randint(49,100)
-  return jsonify({"status" : 200 , "data" : a})
+def index ():
+    return render_template("index.html")
 
-@app.route("/argumentos/ < string : nome>")
-def argumentos (nome: str):
-  return jsonify ({ "status" : 200 ,"data " : nome })
 
-if __name__ == "__main__":
-  app.run(debug=True)
 
+from api import bp 
+app.register_blueprint(bp)

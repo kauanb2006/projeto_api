d5d6dd7cfa69e267571e49ca9f52677bb2dc323a
kauan <kauanbarbosalopes@gmail.com>
2024-05-07 09:57:48 -0300

    rotas

--- api.py
+++ api.py
@@ -5,13 +5,13 @@ bp = Blueprint ("api",__name__)
 def index():
  # Função que retorna o JSON com status 200 e a mensagem "API do SEU_NOME_COMPLETO".
   return {"status": 200, "message": "API do {}".format([ "Kauan Barbosa Lopes"])}
-@bp.route("/aleatorios")
+@bp.route("/api/aleatorios")
 def aleatorios():
   import random
   a = random.randint(49,100)
   return jsonify({"status" : 200 , "data" : a})
 
-@bp.route("/argumentos/ < string : nome>")
+@bp.route("/api/argumentos/ < string : nome>")
 def argumentos (nome: str):
   return jsonify ({ "status" : 200 ,"data " : nome })
 

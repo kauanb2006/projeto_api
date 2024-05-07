fc1e16e7d5e8bbb7f430a59ba0d9a8dd1d8af7a7
kauan <kauanbarbosalopes@gmail.com>
2024-05-07 08:19:50 -0300

    Criando pasta para templates

--- algoritimo.py
+++ algoritimo.py
@@ -11,3 +11,20 @@ tempo_viagem = calcular_tempo_viagem(distancia, velocidade)
 
 print(f"O tempo de viagem será de {tempo_viagem:.2f} horas.")
 
+
+def pedir_nome_motorista():
+  """
+  Função para solicitar ao usuário o nome do motorista e retorna o valor digitado.
+
+  Retorno:
+      str: Nome do motorista digitado pelo usuário.
+  """
+
+  # Solicita o nome do motorista ao usuário
+  nome_motorista = input("Digite o nome do motorista: ")
+
+  return nome_motorista
+
+
+nome_digitado = pedir_nome_motorista()
+print(f"O nome do motorista digitado foi: {nome_digitado}")
